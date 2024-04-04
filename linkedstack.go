fc5d93cdb31e36a1256fc515cb80387f3846ad51
Rafael Franco <rafaelribeirofranco4@gmail.com>
2024-04-04 10:38:43 -0300

    criando estrutura da linked stack

---
+++ linkedstack.go
@@ -0,0 +1,32 @@
+package stack
+
+import "errors"
+
+type Node struct {
+	val  int
+	next *Node
+}
+
+type LinkedStack struct {
+	head *Node
+	size int
+}
+
+func (stack *LinkedStack) Push(val int) {
+}
+
+func (stack *LinkedStack) Pop() (int, error) {
+	return -1, errors.New("error msg")
+}
+
+func (stack *LinkedStack) Peek() (int, error) {
+	return -1, errors.New("error msg")
+}
+
+func (stack *LinkedStack) IsEmpty() bool {
+	return false
+}
+
+func (stack *LinkedStack) Size() int {
+	return 0
+}
